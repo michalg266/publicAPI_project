fd97bb1e4b032578b5b04e06d83cec2892014115
Michal Gordiak <mgordiak@gmail.com>
2024-12-07 22:00:23 +0100

    Generate and save feature implemented

--- index.js
+++ index.js
@@ -1,14 +1,68 @@
 // needed imports
 import express from "express";
+import axios  from "axios";
+import bodyParser from "body-parser";
 
 
 const port = 3000;
 const app=express();
 
-app.use(express.static("public"));
+let choice;
+let data;
+let response;
+let backendJoke;
 
-app.get("/", (req, res) => {
-    res.render("home.ejs");
+let toSave;
+let savedJokes = [];
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", async (req, res) => {
+    response = await axios.get("https://icanhazdadjoke.com/", {
+        headers: {Accept: "application/json"}
+    });
+    backendJoke = response.data.joke;
+    console.log(response);
+    res.render("home.ejs", {
+        joke: backendJoke,
+    })
+});
+
+app.get("/generate", async (req, res) => {
+    response = await axios.get("https://icanhazdadjoke.com/", {
+        headers: {Accept: "application/json"}
+    });
+    backendJoke = response.data.joke;
+    console.log(response);
+    res.render("home.ejs", {
+        joke: backendJoke,
+    })
+});
+
+app.post("/save", (req, res) => {
+    let retreivedJoke = req.body.jokeKey;
+    savedJokes.push(retreivedJoke);
+    console.log("Saved Jokes: ", savedJokes);
+    res.status(200);
+});
+
+app.post("/filter", (req, res) => {
+    choice = req.body.choose_category;
+    console.log("Choice: ", choice);
+    if (choice == 1) {
+        data = "Cat joke";
+    }
+    else if (choice == 2) {
+        data = "Dog joke";
+    }
+    else if (choice  == 3) {
+        data = "Sport joke";
+    }
+    res.render("home.ejs", {
+        joke: data,
+    })
 });
 
 
